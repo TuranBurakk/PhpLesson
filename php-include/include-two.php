a22ab8c54bc5705e7e5da88818bd05a6a453d4a3
burakTuran <buraktt00@gmail.com>
2024-04-10 01:37:51 +0300

    [İnclude]

---
+++ php-include/include-two.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>include2</title>
+</head>
+<body>
+    <?php
+        include_once("include.php");
+        //require_once("include.php"); aynı işlemdir
+        echo $variable;
+    ?>
+    
+</body>
+</html>
---
+++ php-include/include.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>include</title>
+</head>
+
+<body>
+    <?php
+    $variable = "Test";
+    
+    $variable2 = "Test2";
+    
+    
+    ?>
+
+</body>
+
+</html>
